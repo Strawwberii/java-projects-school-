81743de58edbc65d10642d7be627c284c103216c
Rupok ghosh <rupokghosh291@gmail.com>
2023-08-21 17:41:35 -0700

    finish all methods (bug fixing required)

--- ExceptionsAssign/CustomerRating.java
+++ ExceptionsAssign/CustomerRating.java
@@ -1,5 +1,7 @@
+
 import java.util.*;
 import java.io.*;
+
 
 class CustomerRating{
 
@@ -11,57 +13,137 @@ class CustomerRating{
         records = new Customer[5];
     }
 
-    // main method
-
-    public static void main(String[] args){
-
-    }
     // Reading from the file
 
-    public void readRecords() throws IOException{
+    public void readRecords(String file) throws IOException{
 
-        File file = new File("rating.txt");
-        Scanner scanner = new Scanner(file);
+        try(FileReader input = new FileReader(file)){
+        Scanner scanner = new Scanner(input);
 
         while(scanner.hasNextLine()){
             
-            int age = Integer.parseInt(scanner.next());
-            double rating = Double.parseDouble(scanner.next());
+            String info = scanner.nextLine();
+            String[] splitInfo = info.split("\t");
+            
+            int age = Integer.parseInt(splitInfo[0]);
+            double rating = Double.parseDouble(splitInfo[1]);
             addRecord(age, rating);
-        }
+            noOfRecords++;
+        }    
+        scanner.close();
+    }   
+
+
        }
 
 
     // Displaying the records 
 
     public void displayRecord(){
-        
+        System.out.println("    Most updated list of customer ratings   ");
+        System.out.println("----------------------------------------------------------\n");
+        System.out.printf("%-10s %$-10s\n", "Age", "Rating");
+        System.out.println("----------------------------------------------------------\n");
+
+        for( int i=0;i<noOfRecords;i++){
+            Customer customer = records[i];
+            System.out.printf(" %-10d %-10.2f", customer.getAge(), customer.getRating());
+
+        }
 
     }
 
     // add records to the array
 
-    public void addRecord(int age, double rating) throws ArrayIndexOutOfBoundsException{
+    public void addRecord(int age, double rating){
         if(noOfRecords >= 5) {
-            System.out.println("Array is out of bounds!");
+            ;
+            throw new ArrayIndexOutOfBoundsException("Array out of bounds!");
         }
         else{
-            records[noOfRecords] = new Customer(age, rating);
+            records[noOfRecords++] = new Customer(age, rating);
         }
     }
 
-    // write the records to the text file
-
-    public void writeRecords(){
-
-        
-    }
-
-    // get content from user and run the other methods
+    // get input from user and add new records
 
     public void mainMethod(){
 
-        
-    }
+        Scanner scan = new Scanner(System.in);
 
+        // read records from the existing file
+        try {
+            readRecords("rating.txt");
+        }
+        catch(IOException e){
+            System.out.println("Can't read file!");
+        }
+
+        //displays existing records
+        displayRecord();
+
+        // ask user to enter new records (plus letting them quit the app if they want)
+        while(noOfRecords<=5){
+            System.out.println("Enter age[integer], followed by ONE [tab] key, then rating[decimal number] (or type ! to exit)");
+            String input = scan.nextLine();
+            if(input == "!"){
+                break;
+            }
+
+            // add user input to arrays after splitting it up
+    
+            String[] splitInput = input.split("\t");
+            if(splitInput.length <2 || splitInput.length > 2){
+                System.out.println("Error. Input both age and rating only.");
+            }
+
+            try{
+                int age = Integer.parseInt(splitInput[0]);
+                double rating = Double.parseDouble(splitInput[1]);
+                addRecord(age, rating);
+            }
+            catch(NumberFormatException e){
+                System.out.println("Invalid number format.");
+            }
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println(" Array out of bounds.");
+            }
+        }
+
+        //displays updated record
+        displayRecord();
+
+        //Calculate and display average age and raiting
+
+        double totalAge = 0;
+        double totalRating = 0;
+        for (int i = 0; i < noOfRecords; i++) {
+            Customer customer = records[i];
+            totalAge += customer.getAge();
+            totalRating += customer.getRating();
+        }
+
+        double averageAge = totalAge / noOfRecords;
+        double averageRating = totalRating / noOfRecords;
+
+        System.out.printf("Average(Age)= %.2f\n", averageAge);
+        System.out.printf("Average(Rating)= %.2f\n", averageRating);
+
+        // Update the text file with new records 
+
+        try (FileWriter output = new FileWriter("rating.txt")) {
+            for(int i = noOfRecords; i<5; i++){
+                    
+               Customer customer = records[i];
+                output.write(customer.getAge() + "\t" + customer.getRating());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Unable to write to file!");
+        }
+
+        scan.close();
+    }
 }
+
+
--- ExceptionsAssign/Main.java
+++ ExceptionsAssign/Main.java
@@ -1,6 +1,9 @@
 public class Main {
      public static void main (String[] args){
 
-        
+        CustomerRating run = new CustomerRating();
+        run.mainMethod();
+
+
      }
 }
