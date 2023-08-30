74a863fa4506582fca68f7e94c4a3e4de364fee4
Rupok ghosh <rupokghosh291@gmail.com>
2023-08-30 13:28:11 -0700

    add attributes and constructor

---
+++ StacksAssign/Group.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Group<T>{
+
+    //attributes
+    private ArrayList<T> members;
+
+    //constructor
+
+    public Group(){
+        members = new ArrayList<>();
+    }
+    
+
+}
