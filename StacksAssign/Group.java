b4a0c7d53458d1275dbd6ba140d5c1ea9a4eaa35
Rupok ghosh <rupokghosh291@gmail.com>
2023-08-30 14:47:43 -0700

    add all the methods

--- StacksAssign/Group.java
+++ StacksAssign/Group.java
@@ -3,13 +3,36 @@ import java.util.ArrayList;
 public class Group<T>{
 
     //attributes
-    private ArrayList<T> members;
+    private ArrayList<T> aList;
 
     //constructor
-
     public Group(){
-        members = new ArrayList<>();
+        aList = new ArrayList<>();
     }
-    
 
+    // methods
+    public void addToGroup(T member){
+        aList.add(member);
+    }
+
+    public void removeFromGroup(){
+        aList.remove(aList.size() - 1);
+    }
+
+    public void removeFromGroup(T member){
+        aList.remove(member);
+    }
+
+    public int getNUmberOfMembers(){
+        return aList.size();
+    }
+
+    public boolean hasNoMember(){
+        return aList.isEmpty();
+    }
+
+    public String toString(){
+
+
+    }
 }
